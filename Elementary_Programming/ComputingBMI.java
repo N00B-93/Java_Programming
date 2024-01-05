cb594e02064c0857cd93bcb87fe0f8e516cc9d29
N00B-93 <omosheye007@gmail.com>
2024-01-04 17:01:21 -0800

    Add gitignore and source files for Elementary Programming.

---
+++ Elementary_Programming/ComputingBMI.java
@@ -0,0 +1,32 @@
+package Elementary_Programming;
+
+import java.util.Scanner;
+
+/*
+    Body Mass Index (BMI) is a measure of health on weight. It can be calculated by taking your weight in kilograms and
+    dividing by the square of your height in meters. This is a program that prompts the
+    user to enter a weight in pounds and height in inches and displays the BMI.
+ */
+
+public class ComputingBMI
+{
+    public static void main(String[] args)
+    {
+        // Creates a Scanner Object.
+        Scanner input = new Scanner(System.in);
+
+        // Reads in weight.
+        System.out.print("\nEnter weight in pounds: ");
+        double weight = input.nextDouble();
+
+        // Reads in height.
+        System.out.print("\nEnter height in inches: ");
+        double height = input.nextDouble();
+
+        // Converts the weight to kilograms and height to meters and then calculate the BMI.
+        double bmi = (weight * 0.45359) / (Math.pow(height * 0.0254, 2));
+
+        // Displays the result.
+        System.out.printf("\nBMI is: %.2f\n", bmi);
+    }
+}
---
+++ Elementary_Programming/RunwayLength.java
@@ -0,0 +1,33 @@
+package Elementary_Programming;
+
+import java.util.Scanner;
+
+/*
+    This is a program that prompts the user to enter v in meters/second (m/s) and the
+    acceleration a in meters/second squared (m/s2), and displays the minimum runway
+    length.
+ */
+
+public class RunwayLength
+{
+    public static void main(String[] args)
+    {
+        // Creates a Scanner Object.
+        Scanner input = new Scanner(System.in);
+
+        // Reads in the velocity in m/s.
+        System.out.print("\nEnter the velocity in m/s: ");
+        double velocity = input.nextDouble();
+
+
+        // Reads in the acceleration in m/s2.
+        System.out.print("\nEnter the acceleration in m/s2: ");
+        double acceleration = input.nextDouble();
+
+        // Calculates the minimum runway required.
+        double length = Math.pow(velocity, 2) / (2 * acceleration);
+
+        // Displays the result.
+        System.out.printf("\nThe minimum runway length for this airplane is: %.2f\n", length);
+    }
+}
