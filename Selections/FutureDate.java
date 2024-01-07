b047f76737b240e142227602eb255aa0fc7a9fb2
N00B-93 <omosheye007@gmail.com>
2024-01-07 08:47:17 -0800

    Add the  SortThreeIntegers.java, RockPaperScissors.java,  RandomMonth.java, LinearEquation.java,  FutureDate.java, ComputeAndInterpreteBMI.java, CircleInCircle.java source files.

---
+++ Selections/FutureDate.java
@@ -0,0 +1,78 @@
+package Selections;
+
+import java.util.*;
+
+/*
+	This is a program that prompts the user to enter an integer for
+    today’s day of the week (Sunday is 0, Monday is 1, ..., and Saturday is 6). It also prompts
+    the user to enter the number of days after today for a future day and displays
+    the future day of the week.
+ */
+
+public class FutureDate
+{
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+
+		// Reads in the integer values that represents the current day and the number of days elapsed.
+		System.out.print("\nEnter today's day: ");
+		int today = input.nextInt();
+		System.out.print("\nEnter the number of days elapsed since today: ");
+		int daysElapsed = input.nextInt();
+
+		// Switch-Case block that prints out the day based on the value entered by the user.
+		switch (today)
+		{
+			case 0:
+				System.out.print("\nToday is Sunday ");
+				break;
+			case 1:
+				System.out.print("\nToday is Monday ");
+				break;
+			case 2:
+				System.out.print("\nToday is Tuesday ");
+				break;
+			case 3:
+				System.out.print("\nToday is Wednesday ");
+				break;
+			case 4:
+				System.out.print("\nToday is Thursday ");
+				break;
+			case 5:
+				System.out.print("\nToday is Friday ");
+				break;
+			case 6:
+				System.out.print("\nToday is Saturday ");
+				break;
+		}
+
+		// Calculates the future day.
+		int futureDay = daysElapsed % 7;
+
+		// Switch-Case block that prints out the future day.
+		switch (today + futureDay)
+		{
+			case 0:
+				System.out.println("and the future day is Sunday");
+				break;
+			case 1:
+				System.out.println("and the future day is Monday");
+				break;
+			case 2:
+				System.out.println("and the future day is Tuesday");
+				break;
+			case 3:
+				System.out.println("and the future day is Wednesday");
+				break;
+			case 4:
+				System.out.println("and the future day is Thursday");
+				break;
+			case 5:
+				System.out.println("and the future day is Friday");
+				break;
+			case 6:
+				System.out.println("and the future day is Saturday");
+		}
+	}
+}
---
+++ Selections/RockPaperScissors.java
@@ -0,0 +1,72 @@
+package Selections;
+
+import java.util.*;
+
+/*
+	This is a program that plays the popular scissor-rock-paper
+	game. (Scissors can cut a paper, a rock can knock scissors, and a paper can
+	wrap a rock.) The program randomly generates a number 0, 1, or 2 representing
+	scissor, rock, and paper. The program prompts the user to enter a number 0, 1, or
+	2 and displays a message indicating whether the user or the computer wins, loses,
+	or draws.
+ */
+
+public class RockPaperScissors
+{
+	public static void main(String[] args)
+	{
+		// Creates a Random Object.
+		Random rand = new Random();
+
+		// Creates a Scanner Object.
+		Scanner input = new Scanner(System.in);
+
+		// Display the name of the game.
+		System.out.print("\t\tROCK, PAPER, SCISSORS\n\n");
+
+		// Reads in the players choice.
+		System.out.print("\nPlayer plays: ");
+		String player = input.nextLine().toLowerCase();
+
+		// Checks if player enters anything other than 'rock', 'paper', 'scissors' and exits the program if true.
+		if (!(player.equalsIgnoreCase("rock") || player.equalsIgnoreCase("paper")
+				|| player.equalsIgnoreCase("scissors")))
+		{
+			System.out.println("\nInvalid Input!!");
+			System.exit (0);
+			input.close();
+		}
+
+		// Initializes COM choice with an empty string.
+		String COM = "";
+
+		// Randomly generates a number and assigns a choice for COM based on the number using an advanced Switch-Case block.
+		int option = rand.nextInt(3);
+        COM = switch (option)
+		{
+            case 0 -> "rock";
+            case 1 -> "paper";
+            case 2 -> "scissors";
+            default -> COM;
+        };
+
+		// Displays COM choice.
+		System.out.printf("\nCOM plays: %s\n\n", COM);
+
+		// Displays the winner based on the choice played.
+		if (player.equals(COM))
+			System.out.println("\nIt's a tie!!!\n\n");
+		else if (player.equals("rock") && COM.equals("scissors"))
+			System.out.println("\nPlayer Wins!!");
+		else if (player.equals("scissors") && COM.equals("rock"))
+			System.out.println("\nCOM wins!!");
+		else if (player.equals("paper") && COM.equals("rock"))
+			System.out.println("\nPlayer Wins!!");
+		else if (player.equals("rock") && COM.equals("paper"))
+			System.out.println("\nCOM Wins!!");
+		else if (player.equals("scissors") && COM.equals("paper"))
+			System.out.println("\nPlayer Wins!!");
+		else
+			System.out.println("\nCOM Wins!!");
+	}
+}
