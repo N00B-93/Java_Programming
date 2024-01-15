e3f3c09e746ba07f55e37f2002f35087ceced4a0
N00B-93 <omosheye007@gmail.com>
2024-01-15 06:00:44 -0800

    Add the PointInACircle.java source file.

---
+++ Selections/PointInACircle.java
@@ -0,0 +1,27 @@
+package Selections;
+
+import java.util.Scanner;
+public class PointInACircle
+{
+    public static void main(String[] args)
+    {
+        // Creates a Scanner Object.
+        Scanner input = new Scanner(System.in);
+
+        // Prompts the user to enter the coordinates of the center of the circle.
+        System.out.printf("\nEnter the x-coordinate and y-coordinate of the point separated by space: ");
+        double xCoord = input.nextDouble();
+        double yCoord = input.nextDouble();
+
+        // Calculates the distance between the center of the two circle.
+        double distance = Math.sqrt(Math.pow(xCoord - 0, 2) + Math.pow(yCoord - 0, 2));
+
+        // Determines whether the point is inside, outside or on the circle.
+        if (distance > 10.0)
+            System.out.printf("\nPoint (%.2f, %.2f) is not in the circle.\n", xCoord, yCoord);
+        else if (distance < 10.0)
+            System.out.printf("\nPoint (%.2f, %.2f) is in the circle.\n", xCoord, yCoord);
+        else
+            System.out.printf("\nPoint (%.2f, %.2f) is on the circle.\n", xCoord, yCoord);
+    }
+}
