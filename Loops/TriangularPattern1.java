860611eead1e83a14efc2d97193dcd3c61482989
N00B-93 <omosheye007@gmail.com>
2024-01-21 23:47:09 +0100

    Add the TriangularPattern1.java source file.

---
+++ Loops/TriangularPattern1.java
@@ -0,0 +1,30 @@
+package Loops;
+
+/*
+	This program displays the triangular pattern:
+		1
+		1 2
+		1 2 3
+		1 2 3 4
+		1 2 3 4 5
+		1 2 3 4 5 6
+	in the console.
+*/
+
+public class TrianglePattern1
+{
+	public static void main(String... args)
+	{
+		int a = 0, b = 0;
+
+		for (a = 1; a <= 6; a++)
+		{
+			for (b = 1; b <= a; b++)
+			{
+				System.out.printf("%d ", b);
+			}
+			System.out.print("\n");
+		}
+	}
+}
+
