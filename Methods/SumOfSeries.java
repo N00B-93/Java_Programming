eafa9416b98b0dde39bf0e37b9d70c7f5c965d02
N00B-93 <omosheye007@gmail.com>
2024-01-28 19:11:48 +0100

    Add the SumOfSeries.java source file.

---
+++ Methods/SumOfSeries.java
@@ -0,0 +1,28 @@
+package Methods;
+
+/*
+	This program calculates the sum of the series;
+			m(i) = 1/3 + 2/4 + ... + i/i+2
+	for i = 1, 2, 3, ..., 20
+	and displays the result in a tabular form.
+*/
+
+public class SumOfSeries
+{
+	public static void main(String... args)
+	{
+		System.out.println("i\t\tm(i)");
+
+		for (int i = 1; i <= 20; i++)
+			System.out.printf("%d\t\t%.4f\n", i, sumOfSeries(i));
+	}
+
+	public static double sumOfSeries(int numberOfTerms)
+	{
+		double series = 0;
+
+		for (int i = 1; i <= numberOfTerms; i++)
+			series += i / (double)(i + 2);
+		return series;
+	}
+}
