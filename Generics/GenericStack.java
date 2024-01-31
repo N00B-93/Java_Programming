6b0c3b681536b2f1f3f3e67fe36ed32264a79097
N00B-93 <omosheye007@gmail.com>
2024-01-31 02:49:40 -0800

    Add the Generics directory and the GenericStack.java source file.

---
+++ Generics/GenericStack.java
@@ -0,0 +1,96 @@
+package Generics;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/*
+    This is a program that creates a stack of generic Objects named GenericStack which extends the ArrayList class.
+    The user is then prompted to enter five Strings and the Strings are then displayed in reverse.
+ */
+
+/**
+ * The type Generic stack which represents stack of generic Objects.
+ *
+ * @param <E> the type parameter
+ */
+public class GenericStack<E> extends ArrayList
+{
+    /**
+     * Un-parameterized constructor that instantiates a new Generic stack.
+     */
+    public GenericStack(){}
+
+    /**
+     * This returns the element at the top of the stack.
+     *
+     * @return Element at the top of the stack.
+     */
+    public E peek()
+    {
+        return (E) this.get(this.getSize() - 1);
+    }
+
+    /**
+     *Returns the number of elements in the stack.
+     *
+     * @return The number of elements in the stack.
+     */
+    public int getSize()
+    {
+        return this.size();
+    }
+
+    /**
+     * Add an element to the top of the stack.
+     *
+     * @param e The element to be added.
+     */
+    public void push(E e)
+    {
+        this.add(e);
+    }
+
+    /**
+     * Removes the element at the top of the stack.
+     *
+     * @return The element removed.
+     */
+    public E pop()
+    {
+        return (E) this.get(this.getSize() - 1);
+    }
+
+    /**
+     * Checks if the stack is empty or not.
+     *
+     * @return true if the stack is empty and false if the stack is not empty.
+     */
+    @Override
+    public boolean isEmpty()
+    {
+        return super.isEmpty();
+    }
+
+    public static void main(String[] args)
+    {
+        // Creates a GenericStack Object.
+        GenericStack<String> stack1 = new GenericStack<>();
+
+        // Creates a Scanner Object.
+        Scanner input = new Scanner(System.in);
+
+        // Reads in five strings.
+        for (int i = 0; i < 5; i++)
+        {
+            System.out.printf("\nEnter Element %d: ", i + 1);
+            stack1.add(input.nextLine());
+        }
+
+        // Displays the String in reversed order.
+        System.out.println("\nThe Members of the stack displayed in reversed order are: ");
+        for (int i = stack1.getSize() - 1; i >= 0; i--)
+        {
+            System.out.println(stack1.get(i));
+        }
+    }
+}
