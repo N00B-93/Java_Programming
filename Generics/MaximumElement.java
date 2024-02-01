a36ed7c5b1286f5eac619cb08a5fe75890f48fe1
N00B-93 <omosheye007@gmail.com>
2024-02-01 09:44:20 -0800

    Update the documentation in the MaximumElement.java source file.

--- Generics/MaximumElement.java
+++ Generics/MaximumElement.java
@@ -1,5 +1,10 @@
 package Generics;
 
+/*
+    This is a program that uses a function with the header;
+                public static <E extends Comparable<E>> E max(E[] list)
+    to determine the maximum element in an array of generic types.
+ */
 public class MaximumElement
 {
     public static void main(String[] args)
@@ -7,11 +12,20 @@ public class MaximumElement
         // Creates an array of 10 integers.
         Integer[] numbers = {2, 99, 76, 45, 23, 100, 201, 59, 10003, 678};
 
-        // Determines the maximum element.
-        int max = max(numbers);
+        // Creates an array of Strings.
+        String[] words = {"java", "leng", "buffer", "hungry", "pungent"};
+
+        // Determines the maximum element in the Integer array.
+        int maxNumber = max(numbers);
+
+        // Determines the maximum element in the String array.
+        String maxWord = max(words);
 
         // Displays the result.
-        System.out.printf("\nThe maximum element is: %d\n", max);
+        System.out.printf("\nThe maximum element in the integer array is: %d\n", maxNumber);
+
+        // Display the maximum element in the String array.
+        System.out.printf("\nThe maximum element in the word array is: %s\n", maxWord);
     }
 
     /**
