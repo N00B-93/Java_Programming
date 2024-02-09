88b0793cf495a6b6fbf4fb96109f3a7584d5de03
N00B-93 <omosheye007@gmail.com>
2024-02-08 21:25:17 -0800

    Add the CountSingleDigit.java source file.

---
+++ Single_Dimensional_Arrays/CountSingleDigit.java
@@ -0,0 +1,30 @@
+package Single_Dimensional_Arrays;
+
+public class CountSingleDigit
+{
+	public static void main(String[] args)
+	{
+		// Creates an array that can store 100 integers
+		int[] num = new int[100];
+
+		// Creates an array used to store the frequency of numbers.
+		int[] frequency = new int[10];
+
+		// Randomly generates 100 integers and store them in the array.
+		for (int count = 0; count < num.length; count++)
+		{
+			num[count] = (int)(Math.random() * 10);
+		}
+
+		// Determines the frequency of occurrence of each digit and stores them in the frequency array.
+		for (int count = 0; count < 100; count++)
+			++frequency[num[count]];
+
+		// Displays each number and the frequency of its occurrence.
+		System.out.print("\nNumber\t\t\tFrequency\n");
+		for (int count = 0; count < 10; count++)
+		{
+			System.out.printf("%6d\t\t\t%9d\n", count, frequency[count]);
+		}
+	}
+}
