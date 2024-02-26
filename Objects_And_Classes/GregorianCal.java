0e926ada8677b049ad92f2efd58ff74981128fe8
N00B-93 <omosheye007@gmail.com>
2024-02-25 21:27:43 -0800

    Add the GregorianCal.java source file.

---
+++ Objects_And_Classes/GregorianCal.java
@@ -0,0 +1,25 @@
+import java.util.GregorianCalendar;
+
+public class GregorianCal
+{
+	public static void main(String args[])
+	{
+		// Creates a GregorianCalendar Object.
+		GregorianCalendar cal = new GregorianCalendar();
+
+		// Gets the year, day and month.
+		int year = cal.get(GregorianCalendar.YEAR);
+		int day = cal.get(GregorianCalendar.DAY_OF_MONTH);
+		int month = cal.get(GregorianCalendar.MONTH);
+
+		// Displays the current year, month and day.
+		System.out.printf("\nThe current date is: %02d:%02d:%02d\n", day, month, year);
+
+		// Sets the time elapsed to 1234567898765ms.
+		cal.setTimeInMillis(1234567898765L);
+
+		// Displays the day, month and year for the given elapsed time.
+		System.out.printf("\n%02d:%02d:%d\n", cal.get(GregorianCalendar.DAY_OF_MONTH), cal.get(GregorianCalendar.MONTH), cal.get(GregorianCalendar.YEAR));
+
+	}
+}
