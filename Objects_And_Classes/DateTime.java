3d491e0df13ee3601a4eb15c051ca45764df29d8
N00B-93 <omosheye007@gmail.com>
2024-02-25 21:26:00 -0800

    Add the DateTime.java source file.

---
+++ Objects_And_Classes/DateTime.java
@@ -0,0 +1,18 @@
+package Objects_And_Clases;
+
+import java.util.Date;
+
+public class DateTime
+{
+	public static void main(String args[])
+	{
+		// Creates a Date Object.
+		Date date = new Date();
+
+		// Set the time elapsed to 100000000.
+		date.setTime(100_000_000);
+
+		// Displays the date of the time elapsed as a String.
+		System.out.println(date.toString());
+	}
+}
