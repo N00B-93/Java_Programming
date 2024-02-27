0c3d0770ab0673524ea6c966232b43d35433f525
N00B-93 <omosheye007@gmail.com>
2024-02-27 14:04:29 -0800

    Add the Employee.java class file.

---
+++ Inheritance_And_Polymorphism/Employee_And_ProductionWorker/Employee.java
@@ -0,0 +1,96 @@
+package Inheritance_And_Polymorphism.Employee_And_ProductionWorker;
+
+import java.util.Date;
+public class Employee
+{
+    // Attributes of an Employee.
+    private String employeeName, employeeID;
+    private Date dateHired;
+
+    /**
+     * Instantiates a new Employee with the given Name and ID.
+     *
+     * @param employeeName The employee name.
+     * @param employeeID The employee ID.
+     */
+    public Employee(String employeeName, String employeeID)
+    {
+        this.employeeName = employeeName;
+        this.employeeID = employeeID;
+        this.dateHired = new Date();
+    }
+    // No-arg Constructor.
+    public Employee(){}
+
+    /**
+     * Gets employee name.
+     *
+     * @return The employee name.
+     */
+    public String getEmployeeName()
+    {
+        return this.employeeName;
+    }
+
+    /**
+     * Sets employee name to the given value.
+     *
+     * @param newName The new name.
+     */
+    public void setEmployeeName(String newName)
+    {
+        this.employeeName = newName;
+    }
+
+    /**
+     * Gets employee ID.
+     *
+     * @return The employee ID.
+     */
+    public String getEmployeeID()
+    {
+        return this.employeeID;
+    }
+
+    /**
+     * Sets employee ID to the given value.
+     *
+     * @param newName The new ID.
+     */
+    public void setEmployeeID(String newID)
+    {
+        this.employeeName = newID;
+    }
+
+    /**
+     * Sets date hired to the given value.
+     *
+     * @param newDate The new date hired.
+     */
+    public void setDateHired(Date newDate)
+    {
+        this.dateHired = newDate;
+    }
+
+    /**
+     * Gets date hired.
+     *
+     * @return The date hired.
+     */
+    public Date getDateHired()
+    {
+        return dateHired;
+    }
+
+    /**
+     * A String describing the Employee Object.
+     *
+     * @return Description of the Employee Object.
+     */
+    @Override
+    public String toString()
+    {
+        return "Employee Name: " + this.getEmployeeName() + "\n" + "Employee ID: " + this.getEmployeeID() + "\n" +
+                "Date Hired: " + this.getDateHired();
+    }
+}
