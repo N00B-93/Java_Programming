227030d4d795d09a88b89fd32ceb47ae0dc1be52
N00B-93 <omosheye007@gmail.com>
2024-02-27 14:05:02 -0800

    Add the ProductionWorker.java class file.

--- Inheritance_And_Polymorphism/Employee_And_ProductionWorker/ProductionWorker.java
+++ Inheritance_And_Polymorphism/Employee_And_ProductionWorker/ProductionWorker.java
@@ -1,2 +1,67 @@
-package Inheritance_And_Polymorphism.Employee_And_ProductionWorker;public class ProductionWorker {
+package Inheritance_And_Polymorphism.Employee_And_ProductionWorker;
+
+public class ProductionWorker extends Employee
+{
+    // Attributes of a ProductionWorker.
+    int shift;
+    double hourlyRate;
+
+    /**
+     * Instantiates a new Production worker.
+     *
+     * @param name The ProductionWorker's name.
+     * @param employeeID The ProductionWorker's id.
+     * @param shift The ProductionWorker's shift.
+     * @param hourlyRate The ProductionWorker's hourly rate
+     */
+    public ProductionWorker(String name, String employeeID, int shift, double hourlyRate)
+    {
+        super(name, employeeID);
+        this.shift = shift;
+        this.hourlyRate = hourlyRate;
+    }
+
+    /**
+     * Gets the ProductionWorker's shift.
+     *
+     * @return "Day" if the shift is 1, else "Night".
+     */
+    public String getShift()
+    {
+        if (this.shift == 1) return "Day";
+        return "Night";
+
+    }
+
+    /**
+     * Sets shift to the given value.
+     *
+     * @param newShift The new shift.
+     */
+    public void setShift(String newShift)
+    {
+        if (newShift.equalsIgnoreCase("day"))
+            this.shift = 1;
+        this.shift = 2;
+    }
+
+    /**
+     * Gets the hourly rate of the ProductionWorker.
+     *
+     * @return The hourly rate.
+     */
+    public double getHourlyRate()
+    {
+        return this.hourlyRate;
+    }
+
+    /**
+     * Sets hourly rat to the given value.
+     *
+     * @param newRate The ProductionWorker's new hourly rate.
+     */
+    public void setHourlyRate(double newRate)
+    {
+        this.hourlyRate = newRate;
+    }
 }
