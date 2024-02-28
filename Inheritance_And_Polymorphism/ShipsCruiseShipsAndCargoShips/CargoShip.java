4a32e03c2cf74f95eb3752ff5e59a2e69f438c39
N00B-93 <omosheye007@gmail.com>
2024-02-27 17:19:30 -0800

    Add the CargoShip.java class file.

--- Inheritance_And_Polymorphism/ShipsCruiseShipsAndCargoShips/CargoShip.java
+++ Inheritance_And_Polymorphism/ShipsCruiseShipsAndCargoShips/CargoShip.java
@@ -1,2 +1,51 @@
-package Inheritance_And_Polymorphism.ShipsCruiseShipsAndCargoShips;public class CargoShip {
+package Inheritance_And_Polymorphism.ShipsCruiseShipsAndCargoShips;
+
+public class CargoShip extends Ship
+{
+    // Capacity of the CargoShip.
+    private int cargoCapacity;
+
+    /**
+     * Instantiates a new Cargo ship.
+     *
+     * @param shipName The Cargo Ship's name.
+     * @param yearBuilt The year built.
+     * @param cargoCapacity The cargo capacity.
+     */
+    public CargoShip(String shipName, String yearBuilt, int cargoCapacity)
+    {
+        super(shipName, yearBuilt);
+        this.cargoCapacity = cargoCapacity;
+    }
+
+    /**
+     * Sets cargo capacity to the given value.
+     *
+     * @param cargoCapacity The new cargo capacity
+     */
+    public void setCargoCapacity(int cargoCapacity)
+    {
+        this.cargoCapacity = cargoCapacity;
+    }
+
+    /**
+     * Gets the cargo capacity.
+     *
+     * @return The cargo capacity
+     */
+    public int getCargoCapacity()
+    {
+        return this.cargoCapacity;
+    }
+
+    /**
+     * Returns a String description of the Cargo Ship.
+     *
+     * @return The description of the Cargo Ship.
+     */
+    @Override
+    public String toString()
+    {
+        return super.toString() + "Cargo Capacity: " + this.getCargoCapacity() + " tonnes" + "\n";
+    }
 }
