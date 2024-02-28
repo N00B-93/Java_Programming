d6d22b98a954df57340ac6ed9543018258ae6071
N00B-93 <omosheye007@gmail.com>
2024-02-27 17:19:55 -0800

    Add the Main.java source file.

--- Inheritance_And_Polymorphism/ShipsCruiseShipsAndCargoShips/Main.java
+++ Inheritance_And_Polymorphism/ShipsCruiseShipsAndCargoShips/Main.java
@@ -1,2 +1,19 @@
-package Inheritance_And_Polymorphism.ShipsCruiseShipsAndCargoShips;public class Main {
+package Inheritance_And_Polymorphism.ShipsCruiseShipsAndCargoShips;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        // Creates a Ship, CargoShip and CruiseShip Object.
+        Ship ship = new Ship("Demeter", "1656");
+        CargoShip cargoShip = new CargoShip("Orion", "2017", 100);
+        CruiseShip cruiseShip = new CruiseShip("Titanic", "1934", 5000);
+
+        // Creates an array of Ship Objects.
+        Ship[] ships = {ship, cruiseShip, cargoShip};
+
+        // Calls the toString method of each Object.
+        for (int i = 0; i < ships.length; ++i)
+            System.out.println(ships[i].toString());
+    }
 }
