d94148ccbeb381f60e8a0508ff1515b19e09ad50
N00B-93 <omosheye007@gmail.com>
2024-03-10 15:18:30 -0700

    Add the Health.java class file.

--- Abstract_Classes_And_Interfaces/Insurance/Health.java
+++ Abstract_Classes_And_Interfaces/Insurance/Health.java
@@ -1,2 +1,34 @@
-package Abstract_Classes_And_Interfaces.Insurance;public class Health {
+package Abstract_Classes_And_Interfaces.Insurance;
+
+/**
+ * The type Health.
+ */
+public class Health extends Insurance
+{
+    /**
+     * Instantiates a new Health Insurance Object.
+     */
+    public Health()
+    {
+        super("Health");
+        this.setCost();
+    }
+
+    /**
+     * Sets the Monthly cost of the Health Insurance Object to $ 36.00.
+     */
+    @Override
+    public void setCost()
+    {
+        this.monthlyPrice = 36.0;
+    }
+
+    /**
+     * Displays the information of a Health Insurance Object.
+     */
+    @Override
+    public void display()
+    {
+        System.out.printf("\nInsurance Type: %s\nMonthly Cost: $ %.2f\n", this.getInsuranceType(), this.getMonthlyPrice());
+    }
 }
