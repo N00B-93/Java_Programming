a330cb7e33654656288e9656cdd4defc058d5012
N00B-93 <omosheye007@gmail.com>
2024-03-10 15:54:50 -0700

    Add the Main.java source file.

--- Abstract_Classes_And_Interfaces/Turning/Main.java
+++ Abstract_Classes_And_Interfaces/Turning/Main.java
@@ -1,2 +1,19 @@
-package Abstract_Classes_And_Interfaces.Turning;public class Main {
+package Abstract_Classes_And_Interfaces.Turning;
+
+/*
+    This is a program that creates the Objects of the Leaf, Page, and Pancake class, adds them to an array
+    and calls the turn method of each Object.
+ */
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        // Creates an array Of Turner Objects.
+        Turner[] turner = {new Leaf(), new Page(), new Pancake()};
+
+        // Calls the turn method of each Object.
+        for (Turner objects: turner)
+           objects.turn();
+    }
 }
