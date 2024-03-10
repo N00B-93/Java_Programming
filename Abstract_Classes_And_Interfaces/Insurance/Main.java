dee644a5d6b45e8b74da69cd87dd33c578bf4355
N00B-93 <omosheye007@gmail.com>
2024-03-10 15:19:15 -0700

    Add the Main.java source file.

--- Abstract_Classes_And_Interfaces/Insurance/Main.java
+++ Abstract_Classes_And_Interfaces/Insurance/Main.java
@@ -1,2 +1,21 @@
-package Abstract_Classes_And_Interfaces.Insurance;public class Main {
+package Abstract_Classes_And_Interfaces.Insurance;
+
+/*
+    This is a program that creates the Object of a Life and Health Insurance class and then displays
+    information of both Insurance Objects.
+ */
+public class Main
+{
+    public static void main(String[] args)
+    {
+        // Creates an Object of a Life Insurance class.
+        Life life = new Life();
+
+        // Creates an Object of a Health Insurance class.
+        Health health = new Health();
+
+        // Displays information of both Insurance Objects.
+        life.display();
+        health.display();
+    }
 }
