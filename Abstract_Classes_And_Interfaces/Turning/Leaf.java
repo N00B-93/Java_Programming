37efbd704ba9ce91532d47e7efd190e48e07764d
N00B-93 <omosheye007@gmail.com>
2024-03-10 15:53:00 -0700

    Add the Leaf.java class file.

--- Abstract_Classes_And_Interfaces/Turning/Leaf.java
+++ Abstract_Classes_And_Interfaces/Turning/Leaf.java
@@ -1,2 +1,13 @@
-package Abstract_Classes_And_Interfaces.Turning;public class Leaf {
+package Abstract_Classes_And_Interfaces.Turning;
+
+public class Leaf implements Turner
+{
+    /**
+     * Displays a String indicating that a Leaf Object is changing colour.
+     */
+    @Override
+    public void turn()
+    {
+        System.out.println("\nLeaf Changing Colour.");
+    }
 }
