a1cbd444defda44d18437972d20f6535e5962305
N00B-93 <omosheye007@gmail.com>
2024-03-10 15:54:26 -0700

    Add the Pancake.java class file.

--- Abstract_Classes_And_Interfaces/Turning/Pancake.java
+++ Abstract_Classes_And_Interfaces/Turning/Pancake.java
@@ -1,2 +1,13 @@
-package Abstract_Classes_And_Interfaces.Turning;public class Pancake {
+package Abstract_Classes_And_Interfaces.Turning;
+
+public class Pancake implements Turner
+{
+    /**
+     * Display a String indicating a Pancake Object being Turned.
+     */
+    @Override
+    public void turn()
+    {
+        System.out.println("\nFlipping Pancake.");
+    }
 }
