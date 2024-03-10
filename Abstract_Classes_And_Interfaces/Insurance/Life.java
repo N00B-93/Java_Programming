ad3e4946b4914c0b8f9c4fa1e0d40311ca387916
N00B-93 <omosheye007@gmail.com>
2024-03-10 15:18:51 -0700

    Add the Life.java class file.

--- Abstract_Classes_And_Interfaces/Insurance/Life.java
+++ Abstract_Classes_And_Interfaces/Insurance/Life.java
@@ -1,2 +1,31 @@
-package Abstract_Classes_And_Interfaces.Insurance;public class Life {
+package Abstract_Classes_And_Interfaces.Insurance;
+
+/**
+ * The type Life.
+ */
+public class Life extends Insurance
+{
+    public Life()
+    {
+        super("Life");
+        this.setCost();
+    }
+
+    /**
+     * Sets the Monthly cost of the Health Insurance Object to $ 196.00.
+     */
+    @Override
+    public void setCost()
+    {
+        this.monthlyPrice = 196.0;
+    }
+
+    /**
+     * Displays the information of a Health Insurance Object.
+     */
+    @Override
+    public void display()
+    {
+        System.out.printf("\nInsurance Type: %s\nMonthly Cost: $ %.2f\n", this.getInsuranceType(), this.getMonthlyPrice());
+    }
 }
