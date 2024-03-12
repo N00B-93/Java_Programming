f2953bb17ac77f28da065141cc8a5087614f8c79
N00B-93 <omosheye007@gmail.com>
2024-03-12 18:59:18 +0100

    Add the NameAgeAnnualIncome.java source file.

---
+++ Elementary_Programming/NameAgeAnnualIncome.java
@@ -0,0 +1,33 @@
+package Elementary_Programming;
+
+import java.util.Scanner;
+
+/*
+	This is a program that promps the user to enter his/her name, age and desired income and then display this 
+	information in form of a comprehension.
+*/
+
+public class NameAgeAnnualIncome
+{
+	public static void main(String... args)
+	{
+		// Creates a Scanner Object.
+		Scanner input = new Scanner(System.in);
+
+		// Prompts the user to enter his/her name.
+		System.out.print("\nEnter Name: ");
+		String name = input.nextLine();
+
+		// Prompts the user to enter his/her age.
+		System.out.print("\nEnter Age: ");
+		int age = input.nextInt();
+
+		// Prompts the user to enter his/her desired annual income.
+		System.out.print("\nEnter desired Income: ");
+		double desiredIncome = input.nextDouble();
+		
+		// Displays the user's name, age and desired income.
+		System.out.printf("\nMy name is %s, my age is %d and\nI hope to earn $ %.2f per year.\n", name, age, desiredIncome);
+	}
+}
+
