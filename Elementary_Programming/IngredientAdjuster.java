3983c954321b81267e4c0a920ab2e2e09b36ea2e
N00B-93 <omosheye007@gmail.com>
2024-03-13 07:44:49 -0700

    Update the IngredientAdjuster.java source file.

--- Elementary_Programming/IngredientAdjuster.java
+++ Elementary_Programming/IngredientAdjuster.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 
 /*
 	A cookie recipe calls for the following ingredients:
-		•	1.5	cups	of	sugar
-		• 	1	cup	of	butter
-		•	2.75 	cups	of	flour
-The recipe produces 48 cookies with these amounts of the ingredients.
+		•	1.5	cups of sugar
+		• 	1.0	cup	of butter
+		•	2.75 cups of flour
+	The recipe produces 48 cookies with these amounts of the ingredients.
 	This is program that asks the user how many cookies he or she wants to make, and then displays the number of cups of
 	each ingredient needed for the specified number of cookies.
 */
@@ -28,11 +28,10 @@ public class IngredientAdjuster
 		double quantityOfButterForOneCookie = 1.0 / 48;
 		double quantityOfFlourForOneCookie = 2.75 / 48;
 
-		// Displays the quantity of ingredients required to bake the numbwr of cookies entered by the user.
+		// Displays the quantity of ingredients required to bake the number of cookies entered by the user.
 		System.out.printf("\nIngredients required to bake %d cookies:", numberOfCookies);
 		System.out.printf("\n%.2f cups of sugar", quantityOfSugarForOneCookie * numberOfCookies);
 		System.out.printf("\n%.2f cups of butter", quantityOfButterForOneCookie * numberOfCookies);
 		System.out.printf("\n%.2f cups of flour\n", quantityOfFlourForOneCookie * numberOfCookies);
 	}
 }
-
