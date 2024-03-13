561a04a6918b0a76abb5b44f17b221d62a1c1cc5
N00B-93 <omosheye007@gmail.com>
2024-03-13 07:44:13 -0700

    Update the EnergyDrinkConsumption.java source file.

--- Elementary_Programming/EnergyDrinkConsumption.java
+++ Elementary_Programming/EnergyDrinkConsumption.java
@@ -2,7 +2,8 @@ package Elementary_Programming;
 
 /*
 	A soft drink company recently surveyed 12,467 of its customers and found that approximately 14 percent of those surveyed 
-	purchase one or more energy drinks per week. Of those customers who purchase energy drinks, approximately 64 percent of them prefer citrusflavored energy drinks. 
+	purchase one or more energy drinks per week. Of those customers who purchase energy drinks, approximately 64 percent
+	of them prefer citrus flavored energy drinks.
 	This program displays the following:
 		• The approximate number of customers in the survey who	purchase one or	more energy drinks per week.
 		• The approximate number of customers in the survey who prefer citrus-flavored energy drinks.
@@ -23,4 +24,3 @@ public class EnergyDrinkConsumption
 		System.out.printf("\nNumber of customers who preferred citrus flavoured drink: %d\n", customersPreferCitrusDrink);
 	}
 }
-
