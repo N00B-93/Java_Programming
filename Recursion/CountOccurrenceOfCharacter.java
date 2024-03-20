37fbbf82292ffb311146e6dcb1f7428f332ece73
N00B-93 <omosheye007@gmail.com>
2024-03-20 14:00:24 +0100

    Add the CountOccurrenceOfCharacter.java source file.

--- Recursion/CountOccurrenceOfCharacter.java
+++ Recursion/CountOccurrenceOfCharacter.java
@@ -20,10 +20,26 @@ public class CountOccurrenceOfCharacter
         // Prompts the user to enter a Character.
         System.out.printf("\nEnter the character to be counted: ");
         char character = input.nextLine().charAt(0);
+	
+	// Displays the number of occurrence of the character in the String.
+	System.out.printf("\nThe number of occurrence of '%c' in '%s' is: %d\n\n", character, string, count(string, character));
     }
 
+    /**
+     * This returns the number of occurrence of a specific character in a String.
+     * @param string The string to be processed.
+     * @param character The character to be counted.
+     *
+     * @return The number of occurrence of a specific character in a String.
+     */
     public static int count(String string, char character)
     {
-
+	if (string.equals(""))
+		return 0;
+	else if (character == string.charAt(string.length() - 1))
+		return 1 + count(string.substring(0, string.length() - 1), character);
+	else
+		return count(string.substring(0, string.length() - 1), character);
     }
 }
+
