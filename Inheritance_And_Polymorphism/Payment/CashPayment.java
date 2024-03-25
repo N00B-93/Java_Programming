7443ac920c20f89807552a65382b107536f9576c
N00B-93 <omosheye007@gmail.com>
2024-03-24 22:28:07 -0700

    Add the CashPayment.java class file.

--- Inheritance_And_Polymorphism/Payment/CashPayment.java
+++ Inheritance_And_Polymorphism/Payment/CashPayment.java
@@ -1,2 +1,27 @@
-package Inheritance_And_Polymorphism.Payment;public class CashPayment {
+package Inheritance_And_Polymorphism.Payment;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class CashPayment extends Payment
+{
+    /**
+     * Instantiates a new CashPayment Object.
+     *
+     * @param amount The amount.
+     */
+    public CashPayment(double amount)
+    {
+        super(amount);
+    }
+
+    /**
+     * Displays information about the payment type.
+     */
+    @Override
+    public void paymentDetails()
+    {
+        NumberFormat unitedState = NumberFormat.getCurrencyInstance(Locale.US);
+        System.out.println("\nThe Payment is made in cash\nAmount is: " + unitedState.format(this.amount));
+    }
 }
