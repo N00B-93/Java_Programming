ee66710390c2cc95faeccc01f65c24a72f0f3ca9
N00B-93 <omosheye007@gmail.com>
2024-03-29 16:04:47 -0700

    Updated the DistinctNumbers.java source file.

--- Single_Dimensional_Arrays/DistinctNumbers.java
+++ Single_Dimensional_Arrays/DistinctNumbers.java
@@ -3,9 +3,8 @@ package Single_Dimensional_Arrays;
 import java.util.Scanner;
 
 /*
-	This is a program that uses a method with the header;
-				public static void checkDistinct(int[] array, int number)
-	to display the distinct element in an array entered by the user.
+	This is a program that prompts the user to enter a list of 10 numbers and then displays the distinct numbers in the
+	list in the order in which they appear.
  */
 
 public class DistinctNumbers
@@ -18,37 +17,34 @@ public class DistinctNumbers
 		// Creates an array of 10 integers.
 		int[] numbers = new int[10];
 
+		// String variable  to hold distinct numbers.
+		String distinctNumbers = "";
+
 		// Prompts the user to enter the elements of the array.
+		System.out.printf("\nEnter the elements of the array separated by space: ");
 		for (int i = 0; i < numbers.length; i++)
 		{
-			System.out.printf("\nEnter number[%d]: ", i);
 			numbers[i] = input.nextInt();
 		}
 
+		// Appends elements of the number array into a new array only once, repeated elements are ignored.
+		for (int i = 0, j = 0; i < numbers.length; ++i)
+		{
+			if (distinctNumbers.contains(String.valueOf(numbers[i])))
+				continue;
+			else
+				distinctNumbers += String.valueOf(numbers[i]);
+		}
+
+		// Displays the number of distinct numbers.
+		System.out.printf("\nThe number of distinct numbers are: %d\n", distinctNumbers.length());
+
 		// Displays the distinct element in the array.
 		System.out.print("\nThe distinct elements are: ");
-		for (int count = 0; count < 10; count++)
-			checkDistinct(numbers, numbers[count]);
-		System.out.println();
-	}
-
-	/**
-	 * Checks if a number is a distinct element in an array of integers.
-	 *
-	 * @param array The array containing numbers.
-	 * @param number The number to be checked.
-	 *
-	 */
-	public static void checkDistinct(int[] array, int number)
-	{
-		int counter = 0;
-
-		for (int count = 0; count < array.length; count++)
+		for (int count = 0; count < distinctNumbers.length(); count++)
 		{
-			if (array[count] == number)
-				counter++;
+			System.out.printf("%c ", distinctNumbers.charAt(count));
 		}
-		if (counter == 1)
-			System.out.printf("%d ", number);
+		System.out.println();
 	}
 }
