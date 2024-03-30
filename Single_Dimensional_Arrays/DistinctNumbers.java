86e5afaf87467e1403d93648d7147a4b8c94142c
N00B-93 <omosheye007@gmail.com>
2024-03-29 22:09:54 -0700

    Implement a new algorithm to solve the Distinct numbers problem.

--- Single_Dimensional_Arrays/DistinctNumbers.java
+++ Single_Dimensional_Arrays/DistinctNumbers.java
@@ -12,39 +12,46 @@ public class DistinctNumbers
 	public static void main(String[] args)
 	{
 		// Creates a Scanner Object.
-		Scanner input = new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in);
 
 		// Creates an array of 10 integers.
 		int[] numbers = new int[10];
+		int size = 0;
 
-		// String variable  to hold distinct numbers.
-		String distinctNumbers = "";
-
-		// Prompts the user to enter the elements of the array.
-		System.out.printf("\nEnter the elements of the array separated by space: ");
-		for (int i = 0; i < numbers.length; i++)
+		// Prompts the user to enter 10 numbers separated by space.
+		System.out.println("Enter 10 numbers separated by space: ");
+		for (int i = 0; i < 10; i++)
 		{
-			numbers[i] = input.nextInt();
+			// Reads in a number.
+			int number = scanner.nextInt();
+
+			boolean exists = false;
+
+			for (int j = 0; j < size; j++)
+			{
+				// Checks if the number already exists in the array.
+				if (numbers[j] == number)
+				{
+					exists = true;
+					break;
+				}
+			}
+			// Add the number to the array if it's not in the array.
+			if (!exists)
+			{
+				numbers[size] = number;
+				size++;
+			}
 		}
 
-		// Appends elements of the number array into a new array only once, repeated elements are ignored.
-		for (int i = 0, j = 0; i < numbers.length; ++i)
-		{
-			if (distinctNumbers.contains(String.valueOf(numbers[i])))
-				continue;
-			else
-				distinctNumbers += String.valueOf(numbers[i]);
-		}
+		// Displays the number of distinct elements.
+		System.out.println("\nNumber of distinct numbers: " + size);
 
-		// Displays the number of distinct numbers.
-		System.out.printf("\nThe number of distinct numbers are: %d\n", distinctNumbers.length());
-
-		// Displays the distinct element in the array.
-		System.out.print("\nThe distinct elements are: ");
-		for (int count = 0; count < distinctNumbers.length(); count++)
+		// Displays the distinct elements.
+		System.out.println("Distinct numbers in input order:");
+		for (int i = 0; i < size; i++)
 		{
-			System.out.printf("%c ", distinctNumbers.charAt(count));
+			System.out.print(numbers[i] + " ");
 		}
-		System.out.println();
 	}
 }
