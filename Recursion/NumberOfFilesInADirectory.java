7404db736cd357dfeb5ff1f1170b031744a03a4a
N00B-93 <omosheye007@gmail.com>
2024-03-30 10:22:16 -0700

    Add Documentation and fix some logic errors in the NumberOfFilesInADirectory.java source file.

--- Recursion/NumberOfFilesInADirectory.java
+++ Recursion/NumberOfFilesInADirectory.java
@@ -3,46 +3,59 @@ package Recursion;
 import java.io.File;
 import java.util.Scanner;
 
+/*
+    This is a program that prompts the user to enter a directory name and then displays the number of Recursion.files in the
+    directory and its subdirectories.
+ */
+
 public class NumberOfFilesInADirectory
 {
     public static int numberOfFile = 0;
+    
     public static void main(String[] args)
     {
         // Creates a Scanner Object.
         Scanner input = new Scanner(System.in);
 
         // Prompts the user to enter a directory name.
-        System.out.printf("\nEnter a directory name: ");
+        System.out.printf("\nEnter an Absolute pathname to a  directory: ");
         String directoryName = input.nextLine();
 
+        File file = new File(directoryName);
+
         // Checks if the user input is a directory name.
-        if (!new File(directoryName).isDirectory())
+        if (!file.isDirectory())
         {
             System.out.printf("\n%s is not a directory, Try again with a valid directory name.\n", directoryName);
             System.exit(1);
         }
 
-        // Displays the number of files in the directory and it's subdirectories.
-        System.out.printf("\nThe number of files in %s is: %d\n\n", directoryName, numberOfFiles(new File(directoryName)));
-
+        // Displays the number of Recursion.files in the directory and it's subdirectories.
+        System.out.printf("\nThe number of Recursion.files in %s is: %d\n\n", directoryName,
+                numberOfFiles(file));
     }
+
+    /**
+     * This returns the number of Recursion.files in a directory using recursion.
+     * @param directoryName The name of the directory whose number of Recursion.files is to be determined.
+     * @return The number of Recursion.files in the directory.
+     */
     public static int numberOfFiles(File directoryName)
     {
-
         File[] files = directoryName.listFiles();
 
-        for (int i = 0; i < files.length; ++i)
+        if (files != null)
         {
-            if (files[i].isFile())
+            for (File myFile: files)
             {
-                numberOfFile++;
-                return numberOfFiles(files[++i]);
-            }
-            else
-            {
-                File[] dirContent = files[i].listFiles();
-                for (File myFiles: dirContent)
-                    return numberOfFiles(myFiles);
+                if (myFile.isFile())
+                {
+                    numberOfFile++;
+                }
+                else if (myFile.isDirectory())
+                {
+                    numberOfFiles(myFile);
+                }
             }
         }
         return numberOfFile;
