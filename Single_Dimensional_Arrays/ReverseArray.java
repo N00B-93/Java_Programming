3a98186a825f29878d2b3092e330213a35e366d0
N00B-93 <omosheye007@gmail.com>
2024-03-29 18:03:23 -0700

    Change the logic in the ReverseArray.java source file from displaying an array in reverse to actually reversing the array and displaying it.

--- Single_Dimensional_Arrays/ReverseArray.java
+++ Single_Dimensional_Arrays/ReverseArray.java
@@ -11,23 +11,35 @@ public class ReverseArray
 {
 	public static void main(String[] args)
 	{
+		// Creates a Scanner Object.
 		Scanner input = new Scanner(System.in);
+
+		// Creates an array of 10 integers.
 		int[] numbers = new int[10];
 
+		// Reads in 10 integers into the array.
+		System.out.printf("\nEnter the elements of the array separated by space: ");
 		for (int count = 0; count < numbers.length; count++)
 		{
-			System.out.printf("\nEnter number[%d]: ", count);
 			numbers[count] = input.nextInt();
 		}
 
-
+		// Displays the array before reversing.
 		System.out.print("\nArray before: ");
 		for (int count = 0; count < numbers.length; count++)
 			System.out.printf("%d ", numbers[count]);
 
+		// Reverses the array.
+		for (int i = 0, j = numbers.length - 1; j >= (numbers.length / 2); i++, j--)
+		{
+			int temp = numbers[j];
+			numbers[j] = numbers[i];
+			numbers[i] = temp;
+		}
 
+		// Displays the array after reversing.
 		System.out.print("\nArray After: ");
-		for (int count = numbers.length - 1; count >= 0; count--)
+		for (int count = 0; count < numbers.length; count++)
 			System.out.printf("%d ", numbers[count]);
 		
 		System.out.print("\n\n");
