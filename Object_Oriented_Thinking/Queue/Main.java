4d4b27ae8fd2d7d740e2ab65a590b809d21de580
N00B-93 <omosheye007@gmail.com>
2024-03-31 13:12:49 -0700

    Add the Main.java source file containing the main method for the Queue.java class.

--- Object_Oriented_Thinking/Queue/Main.java
+++ Object_Oriented_Thinking/Queue/Main.java
@@ -1,2 +1,26 @@
-package Object_Oriented_Thinking.Queue;public class Main {
+package Object_Oriented_Thinking.Queue;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        // Creates a Queue.
+        Queue queue = new Queue();
+
+        // Displays if the Queue is empty or not.
+        System.out.println("\nIs the Queue empty? " + queue.isEmpty());
+
+        // Adds 20 elements to the Queue.
+        for (int i = 1; i <= 20; i++)
+        {
+            queue.enQueue(i);
+        }
+        System.out.println(queue.getSize());
+        // Pops out all the elements in the Queue and displays them.
+        for (int i = 0; i < queue.getSize(); i++)
+        {
+            System.out.printf("%d ", queue.deQueue());
+        }
+
+    }
 }
