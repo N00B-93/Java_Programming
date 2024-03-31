2b14210c2d0ae68fb9bef0241d48d37d033ac3d9
N00B-93 <omosheye007@gmail.com>
2024-03-31 13:29:38 -0700

    Add documentation to the Main.java source file.

--- Object_Oriented_Thinking/Queue/Main.java
+++ Object_Oriented_Thinking/Queue/Main.java
@@ -1,5 +1,10 @@
 package Object_Oriented_Thinking.Queue;
 
+/*
+    This is a program that creates a Queue Object, added 20 integers into the Queue and then pops the integers one by
+    one and displays them.
+ */
+
 public class Main
 {
     public static void main(String[] args)
@@ -11,16 +16,22 @@ public class Main
         System.out.println("\nIs the Queue empty? " + queue.isEmpty());
 
         // Adds 20 elements to the Queue.
+        System.out.println("\nAdding 20 elements to the Queue...");
         for (int i = 1; i <= 20; i++)
         {
             queue.enQueue(i);
-        }
-        System.out.println(queue.getSize());
-        // Pops out all the elements in the Queue and displays them.
-        for (int i = 0; i < queue.getSize(); i++)
-        {
-            System.out.printf("%d ", queue.deQueue());
+            System.out.println("Added: " + i);
         }
 
+        // Displays if the Queue is empty or not.
+        System.out.println("\nIs the Queue empty? " + queue.isEmpty());
+        System.out.printf("\nThe size of the Queue is: %d\n", queue.getSize());
+
+        // Pops out all the elements in the Queue and displays them.
+        System.out.println("\nPopping elements in the queue...");
+        for (int i = 0; i < queue.getSize(); i++)
+        {
+            System.out.printf("Popped: %d\n", queue.deQueue());
+        }
     }
 }
