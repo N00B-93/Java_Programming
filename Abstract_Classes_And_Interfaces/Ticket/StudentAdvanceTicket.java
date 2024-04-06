f10b9cc45631a4a4221b3a41779fd876180ee26a
N00B-93 <omosheye007@gmail.com>
2024-04-06 16:06:36 -0700

    Add the StudentAdvanceTicket.java class file.

--- Abstract_Classes_And_Interfaces/Ticket/StudentAdvanceTicket.java
+++ Abstract_Classes_And_Interfaces/Ticket/StudentAdvanceTicket.java
@@ -1,2 +1,36 @@
-package Abstract_Classes_And_Interfaces.Ticket;public class StudentAdvanceTicket {
+package Abstract_Classes_And_Interfaces.Ticket;
+
+public class StudentAdvanceTicket extends AdvanceTicket
+{
+    /**
+     * Instantiates a new AdvanceTicket
+     *
+     * @param daysInAdvance Indicates the number of days in advance that the ticket is being purchased
+     */
+    public StudentAdvanceTicket(int daysInAdvance)
+    {
+        super(daysInAdvance);
+    }
+
+    /**
+     * This returns the price of the StudentAdvanceTicket.
+     *
+     * @return The price of the StudentAdvanceTicket.
+     */
+    @Override
+    public double getPrice()
+    {
+        return this.price / 2.0;
+    }
+
+    /**
+     * This returns a String describing the StudentAdvanceTicket.
+     *
+     * @return A String describing the StudentAdvanceTicket.
+     */
+    @Override
+    public String toString()
+    {
+        return super.toString() + " (Student)";
+    }
 }
