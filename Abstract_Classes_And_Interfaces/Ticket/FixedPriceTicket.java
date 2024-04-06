771cca037b710b5bc8e5c38a2305ab8e23c0efa5
N00B-93 <omosheye007@gmail.com>
2024-04-06 16:05:42 -0700

    Add the FixedPriceTicket.java class file.

--- Abstract_Classes_And_Interfaces/Ticket/FixedPriceTicket.java
+++ Abstract_Classes_And_Interfaces/Ticket/FixedPriceTicket.java
@@ -1,2 +1,15 @@
-package Abstract_Classes_And_Interfaces.Ticket;public class FixedPriceTicket {
+package Abstract_Classes_And_Interfaces.Ticket;
+
+public abstract class FixedPriceTicket extends Ticket
+{
+    /**
+     * Instantiates a FixedPriceTicket.
+     *
+     * @param price The price of the Ticket.
+     */
+    public FixedPriceTicket(double price)
+    {
+        super();
+        this.price = price;
+    }
 }
