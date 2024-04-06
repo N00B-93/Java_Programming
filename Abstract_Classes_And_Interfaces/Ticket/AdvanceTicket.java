b197c70cce60dfd7c8148f2da0e1c7047e51a8da
N00B-93 <omosheye007@gmail.com>
2024-04-06 16:07:06 -0700

    Add the AdvanceTicket.java class file.

--- Abstract_Classes_And_Interfaces/Ticket/AdvanceTicket.java
+++ Abstract_Classes_And_Interfaces/Ticket/AdvanceTicket.java
@@ -1,2 +1,39 @@
-package Abstract_Classes_And_Interfaces.Ticket;public class AdvanceTicket {
+package Abstract_Classes_And_Interfaces.Ticket;
+
+public class AdvanceTicket extends Ticket
+{
+    /**
+     * Instantiates a new AdvanceTicket
+     *
+     * @param daysInAdvance Indicates the number of days in advance that the ticket is being purchased
+     */
+    public AdvanceTicket(int daysInAdvance)
+    {
+        if (daysInAdvance >= 10)
+            this.price = 30.0;
+        else
+            this.price = 40.0;
+    }
+
+    /**
+     * This returns the serial number of the AdvanceTicket.
+     *
+     * @return The serial Number of the AdvanceTicket .
+     */
+    @Override
+    public int getSerialNumber()
+    {
+        return this.serialNumber;
+    }
+
+    /**
+     * This returns the price of the AdvanceTicket.
+     *
+     * @return The price of the AdvanceTicket.
+     */
+    @Override
+    public double getPrice()
+    {
+        return this.price;
+    }
 }
