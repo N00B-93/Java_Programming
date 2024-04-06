4fdd3607ab87fc66eb868bea9678f8319ffb7082
N00B-93 <omosheye007@gmail.com>
2024-04-06 16:06:07 -0700

    Add the ComplimentaryTicket.java class file.

--- Abstract_Classes_And_Interfaces/Ticket/ComplimentaryTicket.java
+++ Abstract_Classes_And_Interfaces/Ticket/ComplimentaryTicket.java
@@ -1,2 +1,32 @@
-package Abstract_Classes_And_Interfaces.Ticket;public class ComplimentaryTicket {
+package Abstract_Classes_And_Interfaces.Ticket;
+
+public class ComplimentaryTicket extends FixedPriceTicket
+{
+    /**
+     * Instantiates a ComplimentaryTicket.
+     */
+    public ComplimentaryTicket()
+    {
+        super(0);
+    }
+
+    /**
+     * This returns the serial number of the ComplimentaryTicket.
+     * @return The serial Number of the Complimentary Ticket.
+     */
+    @Override
+    public int getSerialNumber()
+    {
+        return this.serialNumber;
+    }
+
+    /**
+     * This returns the price of the ComplimentaryTicket.
+     * @return The price of the Complimentary Ticket.
+     */
+    @Override
+    public double getPrice()
+    {
+        return this.price;
+    }
 }
