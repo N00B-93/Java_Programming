22b47796e0ae1fd62de91744de1cf34a391a3c64
N00B-93 <omosheye007@gmail.com>
2024-04-09 18:44:36 -0700

    Add the Stock.java class file.

--- Objects_And_Classes/Stock/Main.java
+++ Objects_And_Classes/Stock/Main.java
@@ -1,2 +1,24 @@
-package Objects_And_Classes.Stock;public class Main {
+package Objects_And_Classes.Stock;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        // Creates a Stock Object.
+        Stock stock = new Stock("ORCL", "Oracle Corporation");
+
+        // Sets the previous price.
+        stock.previousClosingPrice = 34.5;
+
+        // Sets the current price.
+        stock.currentPrice = 34.35;
+
+        // Displays the Stock's details.
+        System.out.println("\n\t\tStock Details");
+        System.out.printf("\nStock Name: %s\n", stock.name);
+        System.out.printf("\nSymbol: %s\n", stock.symbol);
+        System.out.printf("\nPrevious Closing Price: $ %.2f\n", stock.previousClosingPrice);
+        System.out.printf("\nCurrent Price: $ %.2f\n", stock.currentPrice);
+        System.out.printf("\nPrice Change Percent: %.2f%%\n", stock.getChangePercent());
+    }
 }
