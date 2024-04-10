23800da710f4b1298c93506eac2ad1e48c90346f
N00B-93 <omosheye007@gmail.com>
2024-04-09 18:43:22 -0700

    Add the Main.java source file.

--- Inheritance_And_Polymorphism/Stack/Main.java
+++ Inheritance_And_Polymorphism/Stack/Main.java
@@ -1,2 +1,57 @@
-package Inheritance_And_Polymorphism.Stack;public class Main {
+package Inheritance_And_Polymorphism.Stack;
+
+import java.util.ArrayList;
+
+/*
+    This is a program that demonstrates how a Stack works using two Objects of the MyStack class.
+ */
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        // Creates two MyStack Objects.
+        MyStack<Integer> myStack1 = new MyStack<>();  // Stores Integers.
+        MyStack<String> myStack2 = new MyStack<>();  // Stores Strings.
+
+        // Adds 5 integers to the Integer Stack.
+        myStack1.push(1);
+        myStack1.push(2);
+        myStack1.push(3);
+        myStack1.push(4);
+        myStack1.push(5);
+
+        // Displays if the Integer Stack is empty or not.
+        System.out.println("\nIs the Integer Stack empty? " + myStack1.isEmpty());
+
+        // Adds 5 Strings to the String Stack;
+        myStack2.push("Bread");
+        myStack2.push("Roast Beef");
+        myStack2.push("Chicken Cutlets");
+        myStack2.push("Mac and Cheese");
+        myStack2.push("Roasted Sausage");
+
+        // Displays if the String Stack is empty or not.
+        System.out.println("\nIs the String Stack empty? " + myStack2.isEmpty());
+
+        // Displays the elements at the top of the Integer and the String Stack.
+        System.out.println("\nElement at the top of the Integer Stack: " + myStack1.peek());
+        System.out.println("\nElement at the top of the String Stack: " + myStack2.peek());
+
+        // Pops all elements in the Integer Stack.
+        System.out.println("\nPopping all Elements in the Integer Stack...");
+        while (!myStack1.isEmpty())
+        {
+            System.out.println(myStack1.pop());
+        }
+        System.out.println("\nIs the Integer Stack empty? " + myStack1.isEmpty());
+
+        // Pops all elements in the String Stack.
+        System.out.println("\nPopping all Elements in the String Stack...");
+        while (!myStack2.isEmpty())
+        {
+            System.out.println(myStack2.pop());
+        }
+        System.out.println("\nIs the Integer Stack empty? " + myStack2.isEmpty());
+    }
 }
