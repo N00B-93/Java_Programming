45deaaa71606c588b8ed806e05bb8c3929135b31
N00B-93 <omosheye007@gmail.com>
2024-05-05 20:43:42 +0100

    Replace for loops with for each loops and convert the if/else condition in the removeDuplicates method to a single if statement.

--- Generics/DistinctElements.java
+++ Generics/DistinctElements.java
@@ -19,10 +19,10 @@ public class DistinctElements
         // Creates a Scanner Object.
         Scanner input = new Scanner(System.in);
 
-        int number = -1;
+        int number;
 
         // Continues to read in numbers till user enters 0.
-        while (number != 0)
+        while (true)
         {
             System.out.printf("\nEnter an integer(press '0' to stop input): ");
             number = input.nextInt();
@@ -35,9 +35,9 @@ public class DistinctElements
         ArrayList<Integer> distinctList = removeDuplicates(list);
 
         // Displays the distinct elements in the list.
-        System.out.println("\nThe distinct elements are: ");
-        for (int i = 0; i < distinctList.size(); i++)
-            System.out.printf("%d ", distinctList.get(i));
+        System.out.print("\nThe distinct elements are: ");
+        for (Integer integer : distinctList)
+            System.out.printf("%d ", integer);
     }
 
 
@@ -52,12 +52,9 @@ public class DistinctElements
     {
         ArrayList<E> distinctList = new ArrayList<>();
 
-        for (int i = 0; i < list.size(); i++)
-        {
-            if (distinctList.contains(list.get(i)))
-                continue;
-            else
-                distinctList.add(list.get(i));
+        for (E e : list) {
+            if (!distinctList.contains(e))
+                distinctList.add(e);
         }
         return distinctList;
     }
