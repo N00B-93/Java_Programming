d38d2cc3df45d6671ceba79bb64c1258943ccbd3
N00B-93 <omosheye007@gmail.com>
2024-05-11 16:13:34 +0100

    Add the Doctor.java class file.

---
+++ Inheritance_And_Polymorphism/Hospital/Doctor.java
@@ -0,0 +1,38 @@
+package Inheritance_And_Polymorphism.Hospital;
+
+public class Doctor extends Person
+{
+    // A Doctor's speciality.
+    private String speciality;
+
+    /**
+     * This returns a Doctor's speciality.
+     *
+     * @return A Doctor's speciality.
+     */
+    public String getSpeciality()
+    {
+        return speciality;
+    }
+
+    /**
+     * Sets a Doctor's speciality to the specified value.
+     *
+     * @param speciality The Doctor's speciality.
+     */
+    public void setSpeciality(String speciality)
+    {
+        this.speciality = speciality;
+    }
+
+    /**
+     * This returns a String description of a Doctor.
+     *
+     * @return String describing a Doctor.
+     */
+    @Override
+    public String toString()
+    {
+        return super.toString() + "\nSpeciality: " + this.speciality + "\n";
+    }
+}
