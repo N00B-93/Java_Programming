840cd6caa1ba84e62da41f6075f11f543224088e
N00B-93 <omosheye007@gmail.com>
2024-05-25 12:02:02 +0100

    Add the InterestBearingAccount.java interface.

---
+++ Abstract_Classes_And_Interfaces/Bank/InterestBearingAccount.java
@@ -0,0 +1,7 @@
+package Abstract_Classes_And_Interfaces.Bank;
+
+public interface InterestBearingAccount
+{
+    // addInterest method to be implemented by classes that implements this interface.
+   void addInterest();
+}
