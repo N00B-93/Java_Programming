67a9a446ed3c405cecd86a1dacbb607751bcc087
N00B-93 <omosheye007@gmail.com>
2024-07-05 15:32:35 +0100

    Add the GreatestCommonDivisor.java source file.

---
+++ Core_Java_Concepts/Loops/GreatestCommonDivisor.java
@@ -0,0 +1,43 @@
+package Core_Java_Concepts.Loops;
+
+import java.util.Scanner;
+
+/**
+	This is a program that prompts the user to enter two positive integers and then determines their
+	greatest common divisor.
+*/
+
+public class GreatestCommonDivisor
+{
+	public static void main(String... args)
+	{
+		// Creates a Scanner Object.
+		Scanner console = new Scanner(System.in);
+		// Prompts the user to enter two integers.
+		System.out.print("\nEnter two positive integers: ");
+		int number1 = console.nextInt();
+		int number2 = console.nextInt();
+
+		// Displays an error message andnterminates the program if the user's input is invalid.
+		if (number1 < 0 || number2 < 0)
+		{
+			System.err.println("\nError: Use positive integers only, Try again.\n");
+			System.exit(0);
+		}
+		
+		// Determines the smallest of the two numbers entered by the user.
+		int smallest = Math.min(number1, number2);
+
+		while (smallest > 0)
+		{
+			if (number1 % smallest == 0 && number2 % smallest == 0)
+			{
+				// Displays the greatest common divisor.
+				System.out.printf("\nGreatest common divisor of %d and %d is: %d\n", number1, number2, smallest);
+				break;  // Breaks out of the loop.
+			}
+			--smallest;  // Decrements smallest.
+		}
+	}
+}
+
