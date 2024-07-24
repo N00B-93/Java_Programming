a106e2f03cee6e5d92685216e9f72973181fb8f1
N00B-93 <omosheye007@gmail.com>
2024-07-24 21:44:16 +0100

    Modify the CalculatingEnergy.java source file
    Remove magic number and use a variable to store amount of the
    calorie to joule conversion factor and also modify the statement
    displaying the final result.

--- Core_Java_Concepts/Elementary_Programming/CalculatingEnergy.java
+++ Core_Java_Concepts/Elementary_Programming/CalculatingEnergy.java
@@ -3,8 +3,9 @@ package Core_Java_Concepts.Elementary_Programming;
 import java.util.Scanner;
 
 /*
-    This is a program that calculates the energy needed
-    to heat water from an initial temperature to a final temperature.
+    This is a program that calculates the energy needed to heat water from an initial temperature to a final temperature.
+    The formula to compute the energy is:
+                    Q = M * (finalTemperature – initialTemperature) * 4184
  */
 
 public class CalculatingEnergy
@@ -13,6 +14,9 @@ public class CalculatingEnergy
     {
         // Creates a Scanner Object.
         Scanner input = new Scanner(System.in);
+
+        // Constant to represent the conversion of calories to joules.
+        int CALORIE_TO_JOULE = 4184;
 
         // Reads in the weight of water in kg.
         System.out.print("\nEnter the amount of water in kg: ");
@@ -27,9 +31,9 @@ public class CalculatingEnergy
         double finalTemperature = input.nextDouble();
 
         // Calculates the energy required.
-        double energyRequired = amountOfWater * (finalTemperature - initialTemperature) * 4184;
+        double energyRequired = amountOfWater * (finalTemperature - initialTemperature) * CALORIE_TO_JOULE;
 
         // Displays the result.
-        System.out.printf("\nThe energy needed is: %.2f\n", energyRequired);
+        System.out.printf("\nThe energy needed is: %.2f joules\n", energyRequired);
     }
 }
