4efdba1435d395f4a2dc45d99e3272bd70746bff
N00B-93 <omosheye007@gmail.com>
2024-07-25 19:41:15 +0100

    Modify the RestaurantBill.java  source file
    Store the tax percent and tip percent in variables instead of
    using magic numbers and corrected spelling error in the class name.

--- Core_Java_Concepts/Elementary_Programming/RestaurantBill.java
+++ Core_Java_Concepts/Elementary_Programming/RestaurantBill.java
@@ -7,31 +7,35 @@ import java.util.Scanner;
 	and the total bill(meal charge plus tax plus tip), then displays the receipt on the screen.
 */
 
-public class ResturantBill
+public class RestaurantBill
 {
 	public static void main(String... args)
 	{
 		// Creates a Scanner Object.
 		Scanner input = new Scanner(System.in);
 
+		// Variables representing the tax and tip percentage.
+		double taxPercent = 0.0675;
+		double tipPercent = 0.2;
+
 		// Prompts the user to enter the meal charge.
 		System.out.print("\nEnter meal charge: $ ");
 		double mealCharge = input.nextDouble();
 
 		// Calculates the tax.
-		double tax = 0.0675 * mealCharge;
+		double tax = taxPercent * mealCharge;
 
 		// Computes the mealCharge with tax.
 		double mealChargeAndTax = tax + mealCharge;
 
 		// Calculates the tip.
-		double tip = 0.2 * mealChargeAndTax;
+		double tip = tipPercent * mealChargeAndTax;
 
 		// Computes the total Bill.
 		double totalBill = mealChargeAndTax + tip;
 
 		// Displays the result.
-		System.out.println("\n##########################Resturant Bill##########################");
+		System.out.println("\n########################## Restaurant Bill ##########################");
 		System.out.printf("\n\t\tMeal Charge: $ %.2f\n", mealCharge);
 		System.out.printf("\n\t\tTax: $ %.2f\n", tax);
 		System.out.printf("\n\t\tTip: $ %.2f\n", tip);
