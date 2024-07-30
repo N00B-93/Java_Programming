a7d6902bf624ff87aaa37a6c3d3176d8ddbae579
N00B-93 <omosheye007@gmail.com>
2024-07-30 21:13:41 +0100

    Optimize code for no root cases
    Modify the code to display a message informing the user about the
    abscence of roots and exit the code if the calculated discriminant is
    zero instead of calculating the root first.

--- Core_Java_Concepts/Selections/QuadraticEquation.java
+++ Core_Java_Concepts/Selections/QuadraticEquation.java
@@ -23,6 +23,12 @@ public class QuadraticEquation
         // Calculates the discriminant.
         double discriminant = Math.pow(b, 2) - (4 * a * c);
 
+        if (discriminant < 0)
+        {
+            System.out.println("\nThe equation has no real roots!");
+            System.exit(0);
+        }
+
         // Calculates the roots.
         double root1 = (-b + Math.sqrt(discriminant)) / (2.0 * a);
         double root2 = (-b - Math.sqrt(discriminant)) / (2.0 * a);
@@ -32,7 +38,5 @@ public class QuadraticEquation
             System.out.printf("\nThe roots are: %.2f and %.2f\n", root1, root2);
         else if (discriminant == 0)
             System.out.printf("\nThe root is %.2f\n", root1);
-        else
-            System.out.println("\nThe equation has no real roots!");
     }
 }
