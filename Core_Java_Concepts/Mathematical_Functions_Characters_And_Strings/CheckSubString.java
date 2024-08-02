0df4b29db47581a3ea536209e356ceaf462f6c7a
N00B-93 <omosheye007@gmail.com>
2024-08-02 10:11:03 +0100

    Update package statement
    Update the package statement, add a block of code to prevent the user
    from using empty Strings and modify the printf statement displaying the
    final result.

--- Core_Java_Concepts/Mathematical_Functions_Characters_And_Strings/CheckSubString.java
+++ Core_Java_Concepts/Mathematical_Functions_Characters_And_Strings/CheckSubString.java
@@ -1,4 +1,4 @@
-package Mathematical_Functions_Characters_And_Strings;
+package Core_Java_Concepts.Mathematical_Functions_Characters_And_Strings;
 
 import java.util.Scanner;
 
@@ -21,10 +21,17 @@ public class CheckSubString
         System.out.printf("\nEnter String 2: ");
         String string2 = input.nextLine();
 
+        // Displays an error message and terminates the program if the user enters empty Strings.
+        if (string1.isEmpty() || string2.isEmpty())
+        {
+            System.out.println("\nError: Use non empty Strings only, Try again.");
+            System.exit(1);
+        }
+
         // Determines whether string2 is a substring of string1 and displays the result.
         if (string1.contains(string2))
-            System.out.printf("\n%s is a substring of %s\n", string2, string1);
+            System.out.printf("\n'%s' is a substring of '%s'\n", string2, string1);
         else
-            System.out.printf("\n%s is not a substring of %s\n", string2, string1);
+            System.out.printf("\n'%s' is not a substring of '%s'\n", string2, string1);
     }
 }
