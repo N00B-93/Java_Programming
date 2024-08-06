7df22e352ac60e87aaf9844c5d7478f9ca3e25a1
N00B-93 <omosheye007@gmail.com>
2024-08-06 22:04:58 +0100

    emove unnecessary initialization
    Remove unnecessary initialization of variables a and b to 0.

--- Core_Java_Concepts/Loops/TriangularPattern2.java
+++ Core_Java_Concepts/Loops/TriangularPattern2.java
@@ -15,15 +15,13 @@ public class TriangularPattern2
 {
 	public static void main(String... args)
 	{
-		int a = 0, b = 0;
+		int a, b;
 
 		for (a = 6; a >= 1; a--)
 		{
 			// Prints a sequence of numbers from b to a.
 			for (b = 1; b <= a; b++)
-			{
 				System.out.printf("%d ", b);
-			}
 
 			// Prints a new line after each row.
 			System.out.print("\n");
