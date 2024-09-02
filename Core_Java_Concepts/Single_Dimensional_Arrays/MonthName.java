26c086b141801f3f5d289203c59d13dcdd1cef9f
N00B-93 <omosheye007@gmail.com>
2024-09-02 23:20:20 +0100

    Remove switch statement displaying final result
    Replace the switch statement displaying the final result with array
    indexing to improve code readability and efficiency.

--- Core_Java_Concepts/Single_Dimensional_Arrays/MonthName.java
+++ Core_Java_Concepts/Single_Dimensional_Arrays/MonthName.java
@@ -1,4 +1,4 @@
-package Single_Dimensional_Arrays;
+package Core_Java_Concepts.Single_Dimensional_Arrays;
 
 import java.util.Scanner;
 
@@ -24,50 +24,12 @@ public class MonthName
 		// Terminates the program if the user enters an invalid month.
 		if (monthNumber < 1 || monthNumber > 12)
 		{
-			System.out.print("\nInvalid input, Enter a nunber from 1 to 12.\n");
+			System.out.print("\nInvalid input, Enter a number from 1 to 12.\n");
 			System.exit(1);
 		}
-		
-		// Displays the month based on the user's input.
-		switch (monthNumber)
-		{
-			case 1:
-				System.out.printf("\nMonth is: %s\n", months[1]);
-				break;
-			case 2:
-				System.out.printf("\nMonth is: %s\n", months[2]);
-				break;
-			case 3:
-				System.out.printf("\nMonth is: %s\n", months[3]);
-				break;
-			case 4:
-				System.out.printf("\nMonth is: %s\n", months[4]);
-				break;
-			case 5:
-				System.out.printf("\nMonth is: %s\n", months[5]);
-				break;
-			case 6:
-				System.out.printf("\nMonth is: %s\n", months[6]);
-				break;
-			case 7:
-				System.out.printf("\nMonth is: %s\n", months[7]);
-				break;
-			case 8:
-				System.out.printf("\nMonth is: %s\n", months[8]);
-				break;
-			case 9:
-				System.out.printf("\nMonth is: %s\n", months[9]);
-				break;
-			case 10:
-				System.out.printf("\nMonth is: %s\n", months[10]);
-				break;
-			case 11:
-				System.out.printf("\nMonth is: %s\n", months[11]);
-				break;
-			case 12:
-				System.out.printf("\nMonth is: %s\n", months[12]);
-				break;
-		}
+
+		// Displays the month based on the user input.
+		System.out.printf("\nMonth is %s\n", months[monthNumber]);
 	}
 }
 
